be7a62ba298f452794fb13c81b618c079b968981
kassHa <katarzynae.lach@gmail.com>
2016-09-20 10:37:53 -0700

    soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    soda_machine = 4
+    #current_inventory_count.soda_machine += 1
+    soda_machine.current_inventory_count
+
   end
 
   def find_soda(soda_brand)
+      find_soda = true
+      args = {"Pepsi", "Coke Zero"}
+      find_soda if find_soda < 0 
   end
 
   def sell(soda_brand)
+    if find_soda == 0
+      soda_machine.sell
+    end 
   end
 
 end
